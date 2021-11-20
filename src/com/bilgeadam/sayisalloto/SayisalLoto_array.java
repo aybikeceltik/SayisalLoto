418c2ea3ca63dd7b995913bccfcafa4f3a906df6
x <celtikkaybike@gmail.com>
2021-11-20 14:50:39 +0300

    sayısal loto claas

--- src/com/bilgeadam/sayisalloto/SayisalLoto_array.java
+++ src/com/bilgeadam/sayisalloto/SayisalLoto_array.java
@@ -13,7 +13,7 @@ public class SayisalLoto_array {
 
 
 
-    public int sayiGetir(int maxSayi)
+    public int sayiGetir()
     {
         // nextInt (90) metodu 0-89 arası üretilir.
         // 1 den başlasın diye 1 ekliyoruz.
@@ -36,30 +36,59 @@ public class SayisalLoto_array {
 
 
     }
-    public void testArraydeVarmı()
-    {
-        int sayilar[] = {13, 45, 67, -4, 41, 70};
-        int sayi = 0;
-        boolean sonuc = arraydeVarmı(sayilar,sayi);
-        if (sonuc==true)
-            System.out.println("Arrayde var!");
-        else
-            System.out.println("Arrayde yok!");
-    }
+//    public void testArraydeVarmı()
+//    {
+//        int sayilar[] = {13, 45, 67, -4, 41, 70};
+//        int sayi = 0;
+//        boolean sonuc = arraydeVarmı(sayilar,sayi);
+//        if (sonuc==true)
+//            System.out.println("Arrayde var!");
+//        else
+//            System.out.println("Arrayde yok!");
+//    }
 
     public static void main(String[] args) {
-        // yeni çılgın sayısal lotoya uygun
+         //yeni çılgın sayısal lotoya uygun
         // 90 sayıdan 6 tane sececegiz
 
-        // sayıları tutacagımız bir array tanımlıyoruz.
-        //int sayilar [] = new int[KAC_SAYI];
-
-        //Döngüyle arraya rastgele sayıılar atalım
-        //for (int i = 0; i < sayilar.length ; i++) {
-
-        //}
-
         SayisalLoto_array loto = new SayisalLoto_array();
-        loto.testArraydeVarmı();
+         //sayıları tutacagımız bir array tanımlıyoruz.
+        int sayilar [] = new int[KAC_SAYI];
+
+       // Döngüyle arraya rastgele sayıılar atalım
+        for (int i = 0; i < sayilar.length ; i++) {
+            int rastgeleSayi = loto.sayiGetir();
+
+            // rastgele sayı arrayde varmı döngüyle tekrar sayı alınacak
+            // arrayde olmayan sayı bulanda kadar döngüden çıkamaz
+            boolean sonuc = loto.arraydeVarmı(sayilar,rastgeleSayi);
+            while (sonuc == true)
+            {
+                rastgeleSayi = loto.sayiGetir();
+                sonuc = loto.arraydeVarmı(sayilar, rastgeleSayi);
+            }
+
+            //her arrayin ilk elamanına yazalım rastgele sayıyı
+            // çünkü sıralnaınca sayı sonlara gidiyor.
+
+            sayilar[0] = rastgeleSayi;
+
+            //&.sayıyı sıfır indekse yazınca tekrar sıralaması için bunu ekledik
+            Arrays.sort(sayilar);
+
+        }
+        //arrayi yazdır.
+        for (int i = 0; i <sayilar.length ; i++) {
+
+
+            if (i == sayilar.length-1)
+                System.out.print(sayilar[i]);
+            else
+                System.out.print(sayilar[i]+"-");
+
+        }
+
+
+        //loto.testArraydeVarmı();
     }
 }
